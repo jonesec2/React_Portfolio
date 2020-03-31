fa5f7e64cf4406b2e11e560d2cb81ba862f3196c
pb-totoro <jonesec2@gmail.com>
2020-03-30 20:26:29 -0400

    Added wrapper and nav bar components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import {
+   BrowserRouter as Router,
+   // Switch,
+   // Route,
+   // Link,
+   // useRouteMatch,
+   // useParams
+ } from "react-router-dom";
 // import logo from './logo.svg';
 import './App.css';
+import Wrapper from './components/Wrapper/Wrapper';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
    return (
-      <div className="App">
-         <h1 className="text-center">Test</h1>
-    </div >
-  );
+      <Router>
+         <div className="App">
+            <Wrapper>
+               <Navbar>
+
+               </Navbar>
+            </Wrapper>
+         </div >
+      </Router>
+   );
 }
 
 export default App;
